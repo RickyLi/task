b642242a3a896b3cb0385d6b16463e36113d537c
RickyLi <lrq348@126.com>
2017-08-23 20:55:34 +0800

    Update problem.php

--- problem.php
+++ problem.php
@@ -6,55 +6,55 @@ class InverseFizzBuzz {
     
     public function sequence() {
         if(empty($this->list)){
-			return '';
+		return '';
+	}
+
+	$mapper = ['fizz' => 3, 'buzz' => 5];
+	$results = [];
+
+	$firstOfList = array_shift($this->list);
+
+	for ($i = 1; $i <= 100; $i++) { 
+		if ($i % $mapper[$firstOfList] == 0) {
+			$results[0][] = $i;
 		}
+	}
 
-		$mapper = ['fizz' => 3, 'buzz' => 5];
-		$results = [];
-
-		$firstOfList = array_shift($this->list);
-
-		for ($i = 1; $i <= 100; $i++) { 
-			if ($i % $mapper[$firstOfList] == 0) {
-				$results[0][] = $i;
-			}
+	foreach ($this->list as $k => $word) {
+		foreach ($results[$k] as $key => $value) {
+			$continue = false;
+			do {
+				++$value;
+				if ($value % $mapper[$this->list[$k]]) {
+					$continue = true;
+				} else {
+					$continue = false;
+					$results[$k+1][] = $value;
+				}
+			} while ($continue);
 		}
+	}
 
-		foreach ($this->list as $k => $word) {
-			foreach ($results[$k] as $key => $value) {
-				$continue = false;
-				do {
-					++$value;
-					if ($value % $mapper[$this->list[$k]]) {
-						$continue = true;
-					} else {
-						$continue = false;
-						$results[$k+1][] = $value;
-					}
-				} while ($continue);
-			}
+	$max = count($this->list);
+	$res = [];
+	foreach ($results[0] as $key => $value) {
+		if ($results[$max][$key] > 100) continue;
+		$num = $results[$max][$key] - $value + 1;
+		$res[$key] = $num;
+	}
+
+	$num = min($res);
+	$result = [];
+	foreach ($res as $key => $value) {
+		if ($value == $num) {
+			$first = $results[0][$key];
+			$last = $results[$max][$key];
+			$result = range($first,$last);
+			break;
 		}
+	}
 
-		$max = count($this->list);
-		$res = [];
-		foreach ($results[0] as $key => $value) {
-			if ($results[$max][$key] > 100) continue;
-			$num = $results[$max][$key] - $value + 1;
-			$res[$key] = $num;
-		}
-
-		$num = min($res);
-		$result = [];
-		foreach ($res as $key => $value) {
-			if ($value == $num) {
-				$first = $results[0][$key];
-				$last = $results[$max][$key];
-				$result = range($first,$last);
-				break;
-			}
-		}
-
-		return $result;
+	return $result;
     }
 }
 
